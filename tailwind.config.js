6a9a2d7574268562629668efff0d395effc0ad11
Dana99Git <106470856+Dana99Git@users.noreply.github.com>
2023-10-14 18:09:06 +0600

    logo website

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+    space: {
+      '50px': '50px'
+    }
+  },
+  plugins: [],
+}
+
